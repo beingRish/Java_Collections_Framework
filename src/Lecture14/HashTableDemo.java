7ffa3c4e68a3c231b83fda9c2b3b520a007c82af
Rishabh Kumar <rishabhjuly11@gmail.com>
2025-05-03 18:33:55 +0530

    Java HashTable

---
+++ src/Lecture14/HashTableDemo.java
@@ -0,0 +1,41 @@
+package Lecture14;
+
+import java.util.HashMap;
+import java.util.Hashtable;
+
+public class HashTableDemo {
+    public static void main(String[] args) {
+        Hashtable<Integer, String> hashtable = new Hashtable<>();
+        hashtable.put(1, "Apple");
+        hashtable.put(2, "Banana");
+        hashtable.put(3, "Cherry");
+        System.out.println(hashtable);
+        System.out.println("Value for key 2: " + hashtable.get(2));
+        System.out.println("Does key 3 exist? " + hashtable.containsKey(3));
+        hashtable.remove(1);
+        System.out.println("After removing key 1: " + hashtable);
+
+
+        Hashtable<Integer, String> map = new Hashtable<>();
+        Thread thread1 = new Thread(() -> {
+            for(int i = 0; i < 1000; i++) {
+                map.put(i, "Thread1");
+            }
+        });
+        Thread thread2 = new Thread(() -> {
+            for(int i = 0; i < 2000; i++) {
+                map.put(i, "Thread1");
+            }
+        });
+        thread1.start();
+        thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Final size of HashMap: " + map.size());
+    }
+}
