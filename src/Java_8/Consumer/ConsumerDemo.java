2895a5f735c1a36a3d05907eb9dd9ce19af057e3
Rishabh Kumar <rishabhjuly11@gmail.com>
2025-05-04 14:16:14 +0530

    Java8 basics | Consumer

---
+++ src/Java_8/Consumer/ConsumerDemo.java
@@ -0,0 +1,20 @@
+package Java_8.Consumer;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ConsumerDemo {
+    public static void main(String[] args) {
+        Consumer<Integer> print = (x) ->System.out.println(x);
+        print.accept(51);   // 51
+
+        List<Integer> list = Arrays.asList(1, 2, 3);
+        Consumer<List<Integer>> printList = x -> {
+            for (int i : x) {
+                System.out.println(i);
+            }
+        };
+        printList.accept(list);
+    }
+}
